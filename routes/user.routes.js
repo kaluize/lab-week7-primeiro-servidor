2527f69503252a72b9c372067222daa28db20cf8
Karen <karentomedi@gmail.com>
2022-11-30 17:41:50 -0300

    done

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,11 @@
-// IMPORTAR BIBLIOTECAS
+// IMPORTAÇÕES
 import express from "express";
+//////////// import UserModel from "../model/user.model.js";
+
 
 // CRIAR VARIÁVEL QUE SERÁ ROTEADOR
 const userRoute = express.Router();
+
 
 // ARQUIVOS RECORTADOS DO INDEX.JS
 // BANCO DE DADOS
@@ -45,34 +48,61 @@ const bancoDeDados = [
   },
 ];
 
+// /////////// Create rota no mongodb - comentar as rotas antigas e substituir por essas
+// // ROTA (1) VERSAO MONGODB - Criar novo processo - PUT
+// userRoute.post("/create-process", async (req, res) => {
+//     try {
+//         const form = req.body;
+//         const newProcess = await UserModel.create(form)
+//         return res.status(200).json(newProcess)
+
+//     // Decorar esse erro! Usar esse padrão sempre
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(500).json({msg: "Algo deu errado na criação do processo"})
+//     } // ou error.erros.properties.message pra mostrar o erro específico
+
+// })
+// // lembrar de atualizar a rota no insomnia pra testar e rodar o node index.js
+
+
+
 // ROTA (1) para acessar constante "bemVindo" como teste -> OK
 // req = request = requsição que vem do cliente
 // res = response = resposta para o cliente
 // retorna uma resposta com status 200 e um json
 userRoute.get("/enap", (req, res) => {
-  const bemVindo = "Bem vindo ao servidor da ENAP turma 92 - Ironhack";
-  return res.status(200).json(bemVindo);
+    const bemVindo = "Bem vindo ao servidor da ENAP turma 92 - Ironhack";
+    console.log("Rota 1 executada com sucesso")
+    return res.status(200).json(bemVindo);
 });
+
 
 // ROTA (2) acessar todo o banco de dados -> OK!
 userRoute.get("/all-processes", (req, res) => {
-  return res.status(200).json(bancoDeDados);
+    console.log("Rota 2 executada com sucesso")
+    return res.status(200).json(bancoDeDados);
 });
+
 
 // ROTA (3) acessar um processo pelo ID - GET - rota: /process/:id -> OK!
 userRoute.get("/process/:id", (req, res) => {
   const { id } = req.params;
   const getProcessById = bancoDeDados.find((process) => process.id === id);
   const index = bancoDeDados.indexOf(getProcessById);
+  console.log("Rota 3 executada com sucesso");
   return res.status(200).json(bancoDeDados[index]);
 });
 
-// ROTA (4) adicionar um novo processo - PUT - rota: /addProcess -> OK!
+
+// ROTA (4) adicionar um novo processo - POST - rota: /addProcess -> OK!
 userRoute.post("/addProcess", (req, res) => {
   const form = req.body;
   bancoDeDados.push(form);
+  console.log("Rota 4 executada com sucesso");
   return res.status(201).json(bancoDeDados);
 });
+
 
 // ROTA (5) adicionar um comentário à array de comentários - PUT - rota: /addComment/:id -> OK!
 userRoute.post("/addComment/:id", (req, res) => {
@@ -81,28 +111,46 @@ userRoute.post("/addComment/:id", (req, res) => {
   const index = bancoDeDados.indexOf(getProcessById);
   const form = req.body;
   bancoDeDados[index].comments.push(form.comment);
+  console.log("Rota 5 executada com sucesso");
   return res.status(201).json(bancoDeDados[index]);
 });
 
-// // ROTA (6) acessar todos os processos em andamento - GET - rota: /status/open -> PENDENTE
-// userRoute.get("/status/open", (req, res) => {
-//   return res.status(200).json();
-// });
 
-// // ROTA (7) acessar todos os processos finalizados - GET - rota: /status/closed -> PENDENTE
-// userRoute.get("/status/closed", (req, res) => {
-//   return res.status(200).json();
-// });
+// ROTA (6) acessar todos os processos em andamento - GET - rota: /status/open -> OK!
+userRoute.get("/status/open", (req, res) => {
+  const statusOpen = bancoDeDados.filter((process) => process.status === "em andamento");
+  console.log("Rota 6 executada com sucesso");
+  return res.status(200).json(statusOpen);
+});
 
-// // ROTA (8) acessar todos os processos DE UM DETERMINADO SETOR - GET - rota: /setor/:nomeSetor -> PENDENTE
-// userRoute.get("/setor/nomeSetor", (req, res) => {
-//   return res.status(200).json();
-// });
 
-// // ROTA (9) acessar um processo escolhido aleatoriamente - GET - rota: /random -> PENDENTE
-// userRoute.get("/random", (req, res) => {
-//   return res.status(200).json();
-// });
+// ROTA (7) acessar todos os processos finalizados - GET - rota: /status/closed -> OK! -> corrigir case sensitive
+userRoute.get("/status/closed", (req, res) => {
+    const statusClosed = bancoDeDados.filter((process) => process.status === "Finalizado");
+    console.log("Rota 7 executada com sucesso");
+    return res.status(200).json(statusClosed);
+  });
+
+
+// ROTA (8) acessar todos os processos DE UM DETERMINADO SETOR - GET - rota: /setor/:nomeSetor -> OK!
+userRoute.get("/setor/:nomeSetor", (req, res) => {
+    const { nomeSetor } = req.params;
+    const processosSetor = bancoDeDados.filter((process) => process.setor === nomeSetor);
+    console.log("Rota 8 executada com sucesso");
+    return res.status(200).json(processosSetor);
+});
+
+
+// ROTA (9) acessar um processo escolhido aleatoriamente - GET - rota: /random -> PENDENTE
+userRoute.get("/random", (req, res) => {
+    const randomIndex = Math.floor(Math.random() * bancoDeDados.length);
+    console.log(bancoDeDados.length);
+    const randomProcess = bancoDeDados[randomIndex];
+    console.log("Rota 9 executada com sucesso");
+    return res.status(200).json(randomProcess);
+});
+
+
 
 // ROTA (10) editar um processo - PUT - rota: /edit/:id -> OK!
 userRoute.put("/edit/:id", (req, res) => {
@@ -113,8 +161,10 @@ userRoute.put("/edit/:id", (req, res) => {
     ...editProcess,
     ...req.body,
   };
+  console.log("Rota 10 executada com sucesso");
   return res.status(200).json(bancoDeDados[index]);
 });
+
 
 // ROTA (11) excluir um objeto - DELETE - rota: /delete -> OK!
 userRoute.delete("/delete/:id", (req, res) => {
@@ -128,8 +178,10 @@ userRoute.delete("/delete/:id", (req, res) => {
 
   const index = bancoDeDados.indexOf(deleteProcessById);
   bancoDeDados.splice(index, 1);
+  console.log("Rota 11 executada com sucesso");
   return res.status(200).json(bancoDeDados);
 });
+
 
 // Exportando as rotas
 export default userRoute;
